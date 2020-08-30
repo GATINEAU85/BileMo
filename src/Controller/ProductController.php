2785f51204b77b0ccb2371ecc909e8fe39bcad3b
GATINEAU <Adrien>
2020-08-30 18:49:29 +0200

    Update for fix Richardson pattern

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -58,7 +58,7 @@ class ProductController extends AbstractController
     
     
     /**
-     * @Route("/products/{page<\d+>?1}", name="getProducts", methods={"GET"})
+     * @Route("/products", name="getProducts", methods={"GET"})
      * @SWG\Response(
      *     response=200,
      *     description="Returns the products list with a pagination by 10 entities.",
@@ -71,7 +71,7 @@ class ProductController extends AbstractController
     {
         $page = $request->query->get('page');
         
-        if($page === null || $page < 1){
+        if(is_null($page) || $page < 1){
             $page = 1 ;
         }
         $limit = 10;
@@ -83,104 +83,4 @@ class ProductController extends AbstractController
             'Content-Type' => 'application/json'
         ]);
     }  
-    
-    /**
-     * @Route("/admin/products", name="addProduct", methods={"POST"})
-     * @SWG\Response(
-     *     response=200,
-     *     description="Returns the products added.",
-     *     @SWG\Schema(ref=@Mod(type=Product::class))
-     * )
-     * @SWG\Tag(name="products")
-     * @Security(name="Bearer")
-     */
-    public function addProduct(Request $request, SerializerInterface $serializer, EntityManagerInterface $em, ValidatorInterface $validator)
-    {
-        $product = $serializer->deserialize($request->getContent(), Product::class, 'json');
-        
-        //TODO - Renvoyer une bonne erreur 
-        $errors = $validator->validate($product);
-        
-        if (count($errors)){
-            $errors = $serializer->serialize($errors, 'json');
-            return new Response($errors, 500, [
-                'Content-Type' => 'application/json'
-            ]);
-        }
-        
-        $em->persist($product);
-        $em->flush();
-        
-        $data = [
-            'status' => 201,
-            'message' => 'This products add is a success.'
-        ];
-        return new JsonResponse($data, 201);
-    }
-    
-    
-    /**
-     * @Route("/admin/products/{id}", name="updateProduct", methods={"PUT"})
-     * @SWG\Response(
-     *     response=200,
-     *     description="Returns the product updated.",
-     *     @SWG\Schema(ref=@Mod(type=Product::class))
-     * )
-     * @SWG\Tag(name="products")
-     * @Security(name="Bearer")
-     */
-    public function updateProduct(Request $request, SerializerInterface $serializer, Product $product, ValidatorInterface $validator, EntityManagerInterface $em)
-    {
-        $productUpdate = $em->getRepository(Product::class)->find($product->getId());
-        $data = json_decode($request->getContent());
-        foreach ($data as $key => $value){
-            if($key && !empty($value)) {
-                if($key !== "id"){
-                    $name = ucfirst($key);
-                    $setter = 'set'.$name;
-                    if($key == "model"){
-                        $model = $em->getRepository(Model::class)->find($product->getModel());
-                        $productUpdate->$setter($model);
-                    }else{
-                        $productUpdate->$setter($value);
-                    }
-                }
-            }
-        }
-        $errors = $validator->validate($productUpdate);
-        if(count($errors)) {
-            $errors = $serializer->serialize($errors, 'json');
-            return new Response($errors, 500, [
-                'Content-Type' => 'application/json'
-            ]);
-        }
-        $em->flush();
-        $data = [
-            'status' => 200,
-            'message' => 'The product is updated.'
-        ];
-        return new JsonResponse($data);
-    }
-    
-    /**
-     * @Route("/admin/products/{id}", name="deleteProduct", methods={"DELETE"})
-     * @SWG\Response(
-     *     response=200,
-     *     description="Returns the product deleted.",
-     *     @SWG\Schema(ref=@Mod(type=Product::class))
-     * )
-     * @SWG\Tag(name="products")
-     * @Security(name="Bearer")
-     */
-    public function deleteProduct(Product $product, EntityManagerInterface $em)
-    {
-        $em->remove($product);
-        $em->flush();
-        
-        $data =  [
-            'status' => 200,
-            'message' => "The product is deleted.",
-        ];
-        return new JsonResponse($data);
-    }
 }
